8febb7867635508d904018906509e4435fc25153
Joe Paris <joe.paris@outlook.com>
2019-02-19 18:37:47 -0800

    removed if __name__ = ...

--- demos/lab_5/house.py
+++ demos/lab_5/house.py
@@ -13,43 +13,42 @@ point at the peak to the corners of the top edge of the house frame.
 """
 from graphics import GraphWin, Line, Point, Rectangle
 
-if __name__ == "__main__":
-    """
-    Here is a really ugly, monolithic function to draw the house.
-    """
-    win = GraphWin("house.py", 800, 800)
-    # draw house frame
-    p1 = win.getMouse()
-    p2 = win.getMouse()
-    house_frame = Rectangle(p2, p1)
-    house_frame.draw(win)
+"""
+Here is a really ugly, monolithic function to draw the house.
+"""
+win = GraphWin("house.py", 800, 800)
+# draw house frame
+p1 = win.getMouse()
+p2 = win.getMouse()
+house_frame = Rectangle(p2, p1)
+house_frame.draw(win)
 
-    # draw door
-    house_width = abs(p1.getX() - p2.getX())
-    house_height = abs(p1.getY() - p2.getY())
-    door_center = win.getMouse()
-    door_width = house_width / 5
-    door_top_left = Point(door_center.getX() - door_width / 2, door_center.getY())
-    door_bottom_right = Point(door_center.getX() + door_width / 2, p1.getY())
-    door = Rectangle(door_top_left, door_bottom_right)
-    door.draw(win)
+# draw door
+house_width = abs(p1.getX() - p2.getX())
+house_height = abs(p1.getY() - p2.getY())
+door_center = win.getMouse()
+door_width = house_width / 5
+door_top_left = Point(door_center.getX() - door_width / 2, door_center.getY())
+door_bottom_right = Point(door_center.getX() + door_width / 2, p1.getY())
+door = Rectangle(door_top_left, door_bottom_right)
+door.draw(win)
 
-    # draw window
-    window_width = door_width / 4
-    window_center = win.getMouse()
-    window = Rectangle(
-        Point(window_center.getX() - window_width, window_center.getY() - window_width),
-        Point(window_center.getX() + window_width, window_center.getY() + window_width),
-    )
-    window.draw(win)
+# draw window
+window_width = door_width / 4
+window_center = win.getMouse()
+window = Rectangle(
+    Point(window_center.getX() - window_width, window_center.getY() - window_width),
+    Point(window_center.getX() + window_width, window_center.getY() + window_width),
+)
+window.draw(win)
 
-    # draw roof
-    peak = win.getMouse()
-    l1 = Line(peak, p2)
-    l2 = Line(peak, Point(p1.getX(), p1.getY() - house_height))
-    l1.draw(win)
-    l2.draw(win)
+# draw roof
+peak = win.getMouse()
+l1 = Line(peak, p2)
+l2 = Line(peak, Point(p1.getX(), p1.getY() - house_height))
+l1.draw(win)
+l2.draw(win)
 
-    # pause so the user can admire their work
-    win.getMouse()
-    win.close
+# pause so the user can admire their work
+win.getMouse()
+win.close
